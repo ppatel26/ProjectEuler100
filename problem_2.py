71b6b267a81382b99997c5f39b6bf2d334a845bb
Parv Patel <parvpatel@Parvs-MBP.local>
2020-01-10 18:59:42 -0800

    Even Fibonacci numbers

---
+++ problem_2.py
@@ -0,0 +1,12 @@
+# Create a fib series (<4M)
+fib = [1,2]
+while fib[-1] < 4e6:
+    fib.append(fib[-1]+fib[-2])
+
+# Sum even valued terms
+total = 0
+for val in fib:
+    if val%2==0:
+        total = total + val 
+
+print(total)
